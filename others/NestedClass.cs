b88a20955256789827980c134ed8d806c50aa8cb
foldedwave <foldedwave@fake.mail>
2023-01-03 01:18:12 +0000

    Added tests for issue 26 and 27

---
+++ others/NestedClass.cs
@@ -0,0 +1,14 @@
+namespace Others;
+
+public class OuterClass
+{
+    public class InnerClass
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
---
+++ others/NestedNamespaceAndClass.cs
@@ -0,0 +1,13 @@
+namespace Others.Namespace;
+
+public class OuterClass
+{
+    public class InnerClass
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
